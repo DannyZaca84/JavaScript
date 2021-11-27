bfa04a44ab3031b470f6c27d7020b6c55efa1122
Daniel Luna Vazquez <d_lunaroja@hotmail.com>
2021-11-26 18:06:05 -0800

    new added file

--- functions/myScript.js
+++ functions/myScript.js
@@ -1,3 +1,8 @@
+// JavaScript Code Blocks 
+
+// JavaScript statements can be grouped together in code blocks, inside curly brakets {...}
+
+
 function externalFunction() {
     document.getElementById("demo8").innerHTML = "Paragraph changed."
 }
